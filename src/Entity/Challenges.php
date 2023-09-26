59b478f8b22b09fc979aa0234ab98d9c64e3fbad
Val <pro.mvaldo@gmail.com>
2023-09-26 13:34:53 +0200

    change : description varchar to longtext

--- src/Entity/Challenges.php
+++ src/Entity/Challenges.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\ChallengesRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\DBAL\Types\Types;
 
 #[ORM\Entity(repositoryClass: ChallengesRepository::class)]
 class Challenges
@@ -16,7 +17,7 @@ class Challenges
     #[ORM\Column(length: 255)]
     private ?string $title = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(type: Types::TEXT)]
     private ?string $description = null;
 
     #[ORM\Column]
