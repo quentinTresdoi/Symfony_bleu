a575409c0dc9293cf9306d634844fa064f9df79c
quentinTresdoi <tresdoi14@gmail.com>
2023-09-28 11:15:26 +0200

    update info profil

--- src/Controller/profileController.php
+++ src/Controller/profileController.php
@@ -4,6 +4,8 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Entity\Challenges;
+use App\Entity\UsersChallenges;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Form\Type\UserType;
@@ -15,12 +17,27 @@ class profileController extends AbstractController
      * @Route("/profil", name="app_profil")
      */
     public function showinformation(){
-            $user = $this->getDoctrine()->getRepository(User::class)->findAll();
-            $em = $this->getDoctrine()->getManager();
-            $em->flush();
-
+        // info user
+            $user= $this->getUser();
+            
+            $roles=$user->getRoles();
+            if($roles = ["ROLE_ADMIN"]){
+                $roles = 'Admin';
+            }else{
+                $roles = 'user';
+            }
+            // challenge validate
+            $userchallenge= new UsersChallenges;
+            // $chanllengevalid= $challenges->getUsersChallenges();
+            // dump($chanllengevalid);
+            // die;
+            $iduser=$userchallenge->getuser();
+            dump($iduser);
+            die;
+            
         return $this->render('profil/profil.html.twig', [
-            'user' => $user
+            'user' => $user,
+            'role' => $roles
         ]);
     }
        
